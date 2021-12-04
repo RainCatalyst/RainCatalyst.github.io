db27cb88e89ce30209847e22c42ebba1d1660619
RainCatalyst <43936298+RainCatalyst@users.noreply.github.com>
2021-12-05 00:34:07 +0300

    undefined

---
+++ index.js
@@ -0,0 +1,32 @@
+import timestamps from './data/timestamps.json' assert { type: "json" };
+// const { timestamps } = data;
+
+var link, comments, video, last_idx;
+const FRAMERATE = 30;
+
+window.onload = function(e){
+    link = document.querySelector("#link");
+    comments = document.querySelector("#comments");
+    video = document.querySelector("#reel");
+    setInterval(updateCaptions, 10);
+}
+
+function updateCaptions() {
+    var idx = 0;
+    var time = video.currentTime;
+
+    for (var i = timestamps.length - 1; i >= 0; i--) {
+        if (time > timestamps[i].time_s + timestamps[i].time_f / FRAMERATE) {
+            idx = i;
+            break;
+        }
+    }
+
+    if (last_idx != idx) {
+        console.log(idx);
+        last_idx = idx;
+        link.href = timestamps[idx].link;
+        link.innerHTML = timestamps[idx].title;
+        comments.innerHTML = '(' + timestamps[idx].comments + ')';
+    }
+}
